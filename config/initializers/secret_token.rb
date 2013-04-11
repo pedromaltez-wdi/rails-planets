6ae10288037aeb22897f3b6071a04d41e7880809
Pedro Maltez <maltezpedro@gmail.com>
2013-04-11 13:35:23 +0100

    Rails app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPlanets::Application.config.secret_token = 'c5140f08bba2960088c84167272168180c9eb5b90f69c7933f98e0685e826d2dbe3651a35d3cc507b50418ee3368fb163058b4fced22f87ec5a65f2ada14f377'
